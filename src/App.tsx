97469e0bf4d69b12bc6f3a37612a9bfe024184d4
Daniel-derf <danielpardinho321@gmail.com>
2025-05-09 13:33:55 -0300

    feat: use queries

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import "./App.css";
+import { useQuery, useMutation } from "@tanstack/react-query";
+
+const POSTS = [
+  { id: 1, title: "Post 1" },
+  { id: 2, title: "Post 2" },
+];
 
 function App() {
+  const postsQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => wait(1000).then(() => [...POSTS]),
+    // queryFn: () => Promise.reject("ERROR"),
+  });
+
+  if (postsQuery.isLoading) return <h1>Loading...</h1>;
+  if (postsQuery.error) return <pre>{JSON.stringify(postsQuery.error)}</pre>;
+
   return (
-    <>
-      <h1>TanStack Query</h1>
-    </>
+    <div>
+      {postsQuery.data?.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+    </div>
   );
+}
+
+function wait(duration: any) {
+  return new Promise((resolve) => setTimeout(resolve, duration));
 }
 
 export default App;
