84f9250a3ade372d940360c0cd6a50ba77349817
Daniel-derf <danielpardinho321@gmail.com>
2025-05-09 13:49:31 -0300

    feat: use mutations

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import "./App.css";
-import { useQuery, useMutation } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 
-const POSTS = [
+const POSTS: any = [
   { id: 1, title: "Post 1" },
   { id: 2, title: "Post 2" },
 ];
 
 function App() {
+  const queryClient = useQueryClient();
+
   const postsQuery = useQuery({
     queryKey: ["posts"],
     queryFn: () => wait(1000).then(() => [...POSTS]),
     // queryFn: () => Promise.reject("ERROR"),
+  });
+
+  const newPostMutation = useMutation({
+    mutationFn: (title: any) => {
+      return wait(1000).then(() => POSTS.push({ id: crypto.randomUUID(), title }));
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["posts"]);
+    },
   });
 
   if (postsQuery.isLoading) return <h1>Loading...</h1>;
@@ -21,6 +32,10 @@ function App() {
       {postsQuery.data?.map((post) => (
         <div key={post.id}>{post.title}</div>
       ))}
+
+      <button disabled={newPostMutation.isPending} onClick={() => newPostMutation.mutate("New Post")}>
+        Add New
+      </button>
     </div>
   );
 }
